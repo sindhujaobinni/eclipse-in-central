08669f73c7f82c98c0a8fce89850545649c4b149
sindhujaobinni <sindhujaobinni@gmail.com>
2025-04-23 21:03:09 +0530

    commit a message

---
+++ src/practice/Demo1.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Demo1 
+{
+	
+	public static void main (String[] args)
+	
+	{
+		int a = 0;
+		int b = 1;
+		int c;
+		
+		for (int i=0; i<10; i++)
+		{
+			int c =a+b;
+			a =b;
+			b =c;
+		
+		}
+			
+	}
+}
